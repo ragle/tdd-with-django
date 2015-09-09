09b6ae356123754e5482734594ae0353aa14af1b
Rob <ragle@sanukcode.net>
2015-09-09 13:10:31 -0400

    integrates bootstrap and satisfies inital style FTs

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,12 @@
 # Functional tests and user story for a simple "hello world" 
 # to-do app created with Django
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
