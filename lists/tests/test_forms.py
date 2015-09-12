14816608fdc4ebb6a167bdbd459c693a7f00dbe9
Rob <ragle@sanukcode.net>
2015-09-11 22:10:34 -0400

    moved data-handling logic out of view, into form

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-
 from lists.forms import EMPTY_ITEM_ERROR, ItemForm
+from lists.models import Item, List
 
 
 class ItemFormTest(TestCase):
@@ -9,6 +9,7 @@ class ItemFormTest(TestCase):
         form = ItemForm()
         self.assertIn('placeholder="Enter a to-do item"', form.as_p())
         self.assertIn('class="form-control input-lg"', form.as_p())
+
 
     def test_form_validation_for_blank_items(self):
         form = ItemForm(data={'text': ''})
@@ -19,3 +20,10 @@ class ItemFormTest(TestCase):
         )
 
 
+    def test_form_save_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, list_)
