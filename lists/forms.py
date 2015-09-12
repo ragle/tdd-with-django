901bcb0ef9f643a2704a9461939c84103559124d
Rob <ragle@sanukcode.net>
2015-09-12 15:37:01 -0400

    adds duplicate error message in form validation logic

--- lists/forms.py
+++ lists/forms.py
@@ -28,10 +28,12 @@ class ItemForm(forms.models.ModelForm):
 
 
 class ExistingListItemForm(ItemForm):
+
     def __init__(self, for_list, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.instance.list = for_list
 
+    
     def validate_unique(self):
         try:
             self.instance.validate_unique()
@@ -39,5 +41,8 @@ class ExistingListItemForm(ItemForm):
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
 
+    
+    def save(self):
+        return forms.models.ModelForm.save(self)
 
 
