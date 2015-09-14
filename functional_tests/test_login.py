09325d6f732e3fbc064da76a4985aa04b7febdc6
Rob <ragle@sanukcode.net>
2015-09-14 16:05:12 -0400

    reorganizes static files & modifies settings appropriately, adds basic login boilerplate to base template

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -27,7 +27,7 @@ class LoginTest(FunctionalTest):
         # and notices a "Sign in" link for the first time
 
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('login').click()
+        self.browser.find_element_by_id('id_login').click()
 
         # A persona login box appears
         self.switch_to_new_window('Mozilla Persona')
@@ -43,7 +43,7 @@ class LoginTest(FunctionalTest):
         self.switch_to_new_window('To-Do')
 
         # She can see that she is logged in
-        self.wait_for_element_with_id('logout')
+        self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn('kevin@mockmyid.com', navbar.text)
 
