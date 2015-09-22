1a120b7121d484fd5aa76714605025db1dab3166
ragle <ragle@sanukcode.net>
2015-09-22 16:58:24 -0400

    adds logout routing and front-end functionality

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -19,8 +19,21 @@ class LoginTest(FunctionalTest):
 
     def wait_for_element_with_id(self, element_id):
         WebDriverWait(self.browser, timeout=30).until(
-            lambda b: b.find_element_by_id(element_id)
+            lambda b: b.find_element_by_id(element_id),
+            'Could not find element with id {}. Page text was: \n{}'.format(
+                element_id, self.browser.find_element_by_tag_name('body').text
+            )
         )
+
+    def wait_to_be_logged_in(self):
+        self.wait_for_element_with_id('id_logout')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn('kevin@mockmyid.com', navbar.text)
+
+    def wait_to_be_logged_out(self):
+        self.wait_for_element_with_id('id_login')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn('kevin@mockmyid.com', navbar.text)
 
     def test_login_with_persona(self):
         # Kevin goes to the awesome superlists site
@@ -42,11 +55,20 @@ class LoginTest(FunctionalTest):
         # The persona window closes
         self.switch_to_new_window('Quotable')
 
-        # She can see that she is logged in
+        # He can see that he is logged in
         self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn('kevin@mockmyid.com', navbar.text)
 
+        # Refreshing the page, he sees it's a real session login,
+        # not just a one-off for that page
+        self.browser.refresh()
+        self.wait_to_be_logged_in()
 
+        # Terrified of this new feature, he reflexively clicks "logout"
+        self.browser.find_element_by_id('id_logout').click()
+        self.wait_to_be_logged_out()
 
-
+        # The "logged out" status also persists after a refresh
+        self.browser.refresh()
+        self.wait_to_be_logged_out()
