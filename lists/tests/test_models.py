de558701da6b2e21e572a401f35973db3d40bfac
ragle <ragle@sanukcode.net>
2015-09-23 15:32:46 -0400

    adds functionality for associating lists with owners to model and view

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -9,6 +9,14 @@ class ListModelTest(TestCase):
         list_ = List.objects.create()
         self.assertEqual(list_.get_absolute_url(), '/lists/%d/' % (list_.id,))
 
+    def test_lists_can_have_owners(self):
+        user = User.objects.create(email='a@b.com')
+        list_ = List.objects.create(owner=user)
+        self.assertIn(list_, user.list_set.all())
+
+    def test_list_owner_is_optional(self)
+        List.objects.create() # should not raise an error
+
 
 class ItemModelTest(TestCase):
 
