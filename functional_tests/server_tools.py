828147dd3599c4835d5d37b4184569ac3a0e24ae
ragle <ragle@sanukcode.net>
2015-09-23 13:46:13 -0400

    creates fabfile for managing staging databse during functional tests

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -1,21 +1,24 @@
 from os import path
 import subprocess
+from os import environ
 THIS_FOLDER = path.dirname(path.abspath(__file__))
 
 def create_session_on_server(host, email):
+    user = environ['LIVESERVERUSER']
     return subprocess.check_output(
             [
                 'fab',
                 'create_session_on_server:email={}'.format(email),
-                '--host={}'.format(host),
+                '--host={}@{}'.format(user,host),
                 '--hide=everything,status',
             ],
             cwd=THIS_FOLDER
     ).decode().strip()
 
 def reset_database(host):
+    user = environ['LIVESERVERUSER']
     subprocess.check_call(
-            ['fab', 'reset_database', '--host{}'.format(host)],
+            ['fab', 'reset_database', '--host={}@{}'.format(user,host)],
             cwd=THIS_FOLDER
     )
 
